50b8d5f981e69d1337f014a69426094ca78a1134
Rankarusu <darkigfx@gmail.com>
2022-07-31 17:33:35 +0200

    added sticker db commands

--- src/utils/db-utils.ts
+++ src/utils/db-utils.ts
@@ -1,8 +1,10 @@
-import { Reminder } from '@prisma/client';
+import { Reminder, Sticker } from '@prisma/client';
 import { Db } from '../services';
 import { Snowflake } from 'discord.js';
 
 export class DbUtils {
+  //reminders
+
   public static async getAllReminders(): Promise<Reminder[]> {
     const reminders = await Db.reminder.findMany();
     return reminders;
@@ -36,6 +38,44 @@ export class DbUtils {
     interactionIds: Snowflake[]
   ): Promise<void> {
     await Db.reminder.deleteMany({
+      where: {
+        interactionId: {
+          in: interactionIds,
+        },
+      },
+    });
+  }
+
+  //stickers
+  public static async getAllStickers(): Promise<Sticker[]> {
+    const stickers = await Db.sticker.findMany();
+    return stickers;
+  }
+
+  public static async getStickersByGuildId(
+    guildId: Snowflake
+  ): Promise<Sticker[]> {
+    const stickers = await Db.sticker.findMany({
+      where: { guildId },
+      orderBy: { stickerName: 'asc' },
+    });
+    return stickers;
+  }
+
+  public static async createSticker(data: Sticker): Promise<void> {
+    await Db.sticker.create({ data });
+  }
+
+  public async deleteStickerById(interactionId: Snowflake) {
+    await Db.sticker.delete({
+      where: {
+        interactionId,
+      },
+    });
+  }
+
+  public async deleteStickersById(interactionIds: Snowflake[]) {
+    await Db.sticker.deleteMany({
       where: {
         interactionId: {
           in: interactionIds,

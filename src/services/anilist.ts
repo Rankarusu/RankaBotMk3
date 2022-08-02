52be2e67c3ecb0c8d1ec9f2b79b02f5e1803b0a8
Rankarusu <darkigfx@gmail.com>
2022-08-02 14:44:45 +0200

    made Anilist a singleton that updates itself

--- src/services/anilist.ts
+++ src/services/anilist.ts
@@ -1,55 +1,62 @@
 import axios from 'axios';
+import * as cron from 'node-cron';
+import { Logger } from '.';
+import LogMessages from '../../logs/logs.json';
 import { AniListGQLItem, MediaType } from '../models/anilist';
 import { DateUtils } from '../utils';
-const query = `
-query ($start: Int, $end: Int) {
-	Page(page: 1, perPage: 50) {
-		airingSchedules(
-			airingAt_greater: $start
-			airingAt_lesser: $end
-			sort: TIME
-		) {
-			episode
-			airingAt
-			media {
-				title {
-					native
-					romaji
-					english
-				}
-				type
-				countryOfOrigin
-				format
-			}
-		}
-	}
-}
-`;
 
-export class AniList {
-  private static async getAiringPerDay(start: number, end: number) {
-    const data = await axios
-      .post('https://graphql.anilist.co', {
-        headers: {
-          Accept: 'application/json',
-          'User-Agent': 'axios',
-        },
-        query: query,
-        variables: { start, end },
-      })
-      .then((res) => {
-        // console.log(res.data.data.Page.airingSchedules);
-        const entries = res.data.data.Page.airingSchedules.filter(
-          (entry: AniListGQLItem) =>
-            entry.media.countryOfOrigin !== 'CN' &&
-            entry.media.type === MediaType.ANIME
-        );
-        return entries;
-      });
-    return data;
+class AniList {
+  //implementing a Singleton that automatically gets new data from AniList.
+  private schedule: { day: string; airing: AniListGQLItem[] }[];
+
+  private static _instance: AniList;
+
+  private constructor() {
+    //get data once and then in intervals.
+    this.updateSchedule(this.getTimestamps());
+
+    cron.schedule('0 * * * *', async () => {
+      const timestamps = this.getTimestamps();
+      await this.updateSchedule(timestamps);
+    });
+    Logger.info(
+      LogMessages.info.cronInfo.replaceAll(
+        '{TEXT}',
+        'Anilist scheduler started'
+      )
+    );
   }
 
-  public static getTimestamps() {
+  public static get Instance() {
+    return this._instance || (this._instance = new this());
+  }
+
+  private query = `
+  query ($start: Int, $end: Int) {
+    Page(page: 1, perPage: 50) {
+      airingSchedules(
+        airingAt_greater: $start
+        airingAt_lesser: $end
+        sort: TIME
+      ) {
+        episode
+        airingAt
+        media {
+          title {
+            native
+            romaji
+            english
+          }
+          type
+          countryOfOrigin
+          format
+        }
+      }
+    }
+  }
+  `;
+
+  private getTimestamps() {
     const timestamps = [];
     // timestamps.push(time / 1000);
     let end: number;
@@ -65,9 +72,29 @@ export class AniList {
     return timestamps;
   }
 
-  public static async getWeeklySchedule(
-    timestamps: { start: number; end: number }[]
-  ) {
+  private async getAiringPerDay(start: number, end: number) {
+    const data = await axios
+      .post('https://graphql.anilist.co', {
+        headers: {
+          Accept: 'application/json',
+          'User-Agent': 'axios',
+        },
+        query: this.query,
+        variables: { start, end },
+      })
+      .then((res) => {
+        // console.log(res.data.data.Page.airingSchedules);
+        const entries = res.data.data.Page.airingSchedules.filter(
+          (entry: AniListGQLItem) =>
+            entry.media.countryOfOrigin !== 'CN' &&
+            entry.media.type === MediaType.ANIME
+        );
+        return entries;
+      });
+    return data;
+  }
+
+  private async updateSchedule(timestamps: { start: number; end: number }[]) {
     const entries = await Promise.all(
       timestamps.map(async (timestamp) => {
         const entry = await this.getAiringPerDay(
@@ -83,6 +110,11 @@ export class AniList {
         };
       })
     );
-    return entries;
+    this.schedule = entries;
+  }
+
+  public getSchedule() {
+    return this.schedule;
   }
 }
+export const aniList = AniList.Instance;

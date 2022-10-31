d6237b26da06a5e823741fd53c7376c01a0a11d1
Rankarusu <darkigfx@gmail.com>
2022-10-31 15:41:16 +0100

    test: add tests for lewds command

---
+++ tests/commands/nsfw/lewds.test.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { LewdsCommand } from '../../../src/commands';
+import { lewds } from '../../../src/services';
+import { InteractionUtils } from '../../../src/utils';
+import { CommandTestHelper } from '../helper';
+
+const input = [{ name: 'amount', type: 10, value: 10 }];
+
+describe('Lewds', () => {
+  const helper = new CommandTestHelper(new LewdsCommand());
+
+  beforeAll(async () => {
+    // we start and stop the service so we have data to work with
+    await lewds.start();
+    lewds.stop();
+  });
+
+  beforeEach(() => {
+    helper.resetInput();
+    jest.restoreAllMocks();
+  });
+
+  it('should issue a warning if no more lewds are available', async () => {
+    helper.setInput(input);
+    jest.spyOn(lewds, 'getLewdsFromStash').mockImplementationOnce(() => []);
+
+    await helper.executeWithWarning();
+  });
+
+  it('should not issue a warning on valid input', async () => {
+    helper.setInput(input);
+
+    await helper.executeWithoutWarning();
+  });
+
+  it('should not throw an error on valid input', async () => {
+    helper.setInput(input);
+
+    await helper.executeWithoutError();
+  });
+
+  it('should call InteractionUtils.send on valid input', async () => {
+    helper.setInput(input);
+
+    await helper.executeInstance();
+    helper.expectSend();
+  });
+
+  it('should group messages and send them in chunks', async () => {
+    helper.setInput(input);
+
+    await helper.executeInstance();
+    expect(InteractionUtils.send).toHaveBeenCalledTimes(
+      Math.ceil((input[0].value as number) / 5)
+    );
+  });
+});
